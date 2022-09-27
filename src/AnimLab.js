ed0d2afdcfcfe4fe357a9c7a522da7d41c84b06b
Onur Ucar <onurucar@iu.edu>
2022-09-27 18:22:28 -0400

    initial commit

---
+++ src/AnimLab.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
+import "./main.css";
+
+export default function AnimLab(){
+    const [cards, setCards] = useState([]);
+    
+    // on mount
+    useEffect(() => {
+
+    let suits = ["A", "B", "C"];
+    let genCards = suits.flatMap((suit) => {
+        return [1, 2, 3, 4, 5, 6, 7].map((val) => {
+            return {val: suit + val, active: true};
+        });
+    });
+    console.log(cards)
+    setCards(genCards);
+    }, [])
+
+    // rendering stuff
+    let cardEls = cards.map((card, index) => {
+        let delay = index * 100;
+        return (
+            <CSSTransition 
+                key={card.val} 
+                appear={true} 
+                in={card.active} 
+                timeout={{ enter: delay, exit: 400 }} 
+                classNames="card" 
+            >
+                <div 
+                    className="card" 
+                    style={{transitionDelay: `${delay}ms`}}
+                    onClick={() => { 
+                        cards[index].active = false;
+                        setCards([...cards]);
+                    }}
+                >
+                     {card.val}
+                </div>
+            </CSSTransition>
+        );
+    });
+
+    return (
+        <div>
+            <div className="cardStack">{cardEls}</div>
+        </div>
+    );
+}
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AnimLab from "./AnimLab";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Animation Lab</h1>
+      <AnimLab/>
+    </div>
+  );
+}
+
+export default App;
